a63f37ab3dd2c756e4830943537c54ccbe42c57c
Adhamtaher <97228592+Adhamtaher@users.noreply.github.com>
2023-05-21 03:44:41 +0300

    .

--- app/src/main/java/com/example/myapplication/fragment/patient/medicalhistory/doctor/DoctorDetalisHistory.kt
+++ app/src/main/java/com/example/myapplication/fragment/patient/medicalhistory/doctor/DoctorDetalisHistory.kt
@@ -51,6 +51,22 @@ class DoctorDetalisHistory : Fragment() {
             dialog.show()
         }
         binding.Download.setOnClickListener {
+            val uri = Uri.parse("https://docs.google.com/document/d/1er8avOFqmEb5thwdLLUBFAVHr7ha0VbknVk42qagOiE/edit?usp=share_link")
+            val request = DownloadManager.Request(uri)
+
+            // Set the destination directory and filename for the downloaded file
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "My report.docx")
+
+            // Set the title and description for the notification that appears during the download
+            request.setTitle("Downloading Word document")
+            request.setDescription("Please wait while your file is being downloaded.")
+
+            // Get a reference to the system's DownloadManager and enqueue the request
+            val downloadManager = requireContext().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+            downloadManager.enqueue(request)
+
+            // Show a toast message indicating that the download has started
+            Toast.makeText(context, "Download started", Toast.LENGTH_SHORT).show()
         }
         binding.backButton.setOnClickListener {
             activity?.onBackPressed()
